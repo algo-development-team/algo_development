0020cd278bc295a62d8b0c708521d2181a0c0ad5
Hokyung (Andy) Lee <techandy42@gmail.com>
2022-11-05 23:33:02 -0400

    rearranged user info to a separate doc; refresh token now stored in the userInfo doc

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -7,8 +7,17 @@ import {
   signOut,
   signInWithRedirect,
 } from 'firebase/auth'
-import { onSnapshot, updateDoc } from 'firebase/firestore'
+import {
+  onSnapshot,
+  getDocs,
+  addDoc,
+  updateDoc,
+  collection,
+  query,
+  where,
+} from 'firebase/firestore'
 import { getAuth, updateProfile } from 'firebase/auth'
+import { db } from '_firebase'
 import { Navigate, useNavigate } from 'react-router-dom'
 
 export const AuthContext = createContext()
@@ -51,9 +60,13 @@ export const AuthProvider = ({ children }) => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user // user contains the refresh token
-        setCurrentUser(user)
-        localStorage.setItem('userAuth', JSON.stringify(user))
-        console.log(JSON.stringify(user))
+        const userData = {
+          displayName: user.displayName,
+          email: user.email,
+          id: user.uid,
+        }
+        setCurrentUser(userData)
+        localStorage.setItem('userAuth', JSON.stringify(userData))
         navigate('/app/Inbox')
       })
       .catch((error) => {
@@ -75,43 +88,94 @@ export const AuthProvider = ({ children }) => {
   }
 
   useEffect(() => {
-    const getMissingFieldValuePairsInUser = (snapshotData) => {
+    const getDefaultUserInfo = (refreshToken) => {
       const defaultRankings = new Array(24).fill(50)
-      const fieldValuePairs = [
-        ['refreshToken', null],
-        ['workTimeRange', '9:00-17:00'],
-        ['sleepTimeRange', '23:00-07:00'],
-        ['workDays', [false, true, true, true, true, true, false]],
-        ['isSetup', false],
-        ['calendarId', null],
-        ['checklist', []],
-        ['urgentRankingsWw', defaultRankings],
-        ['deepRankingsWw', defaultRankings],
-        ['shallowRankingsWw', defaultRankings],
-        ['urgentRankingsPw', defaultRankings],
-        ['deepRankingsPw', defaultRankings],
-        ['shallowRankingsPw', defaultRankings],
-        ['urgentRankingsPnw', defaultRankings],
-        ['deepRankingsPnw', defaultRankings],
-        ['shallowRankingsPnw', defaultRankings],
-      ]
-      let missingFieldValuePairs = fieldValuePairs.filter(
-        (fieldValuePair) => !snapshotData[fieldValuePair[0]],
-      )
-      missingFieldValuePairs = Object.fromEntries(missingFieldValuePairs)
-      return missingFieldValuePairs
+      const defaultUserInfo = {
+        refreshToken: refreshToken,
+        workTimeRange: '9:00-17:00',
+        sleepTimeRange: '23:00-07:00',
+        workDays: [false, true, true, true, true, true, false],
+        isSetup: false,
+        calendarId: null,
+        checklist: [],
+        urgentRankingsWw: defaultRankings,
+        deepRankingsWw: defaultRankings,
+        shallowRankingsWw: defaultRankings,
+        urgentRankingsPw: defaultRankings,
+        deepRankingsPw: defaultRankings,
+        shallowRankingsPw: defaultRankings,
+        urgentRankingsPnw: defaultRankings,
+        deepRankingsPnw: defaultRankings,
+        shallowRankingsPnw: defaultRankings,
+      }
+      return defaultUserInfo
     }
+
+    const getUserInfo = async (userId) => {
+      try {
+        const userInfoQuery = await query(
+          collection(db, 'user', `${userId}/userInfo`),
+        )
+        const userInfoDocs = await getDocs(userInfoQuery)
+        const userInfoDocList = []
+        userInfoDocs.forEach((userInfoDoc) => {
+          userInfoDocList.push(userInfoDoc)
+        })
+        if (userInfoDocs.empty === true) {
+          return { empty: true, userInfoDoc: null, failed: false }
+        }
+        return { empty: false, userInfoDoc: userInfoDocList[0], failed: false }
+      } catch (error) {
+        console.log(error)
+        return { empty: true, userInfoDoc: null, failed: true }
+      }
+    }
+
+    const populateUserInfo = async (userId, refreshToken) => {
+      const defaultUserInfo = getDefaultUserInfo(refreshToken)
+      try {
+        await addDoc(
+          collection(db, 'user', `${userId}/userInfo`),
+          defaultUserInfo,
+        )
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const updateRefreshToken = async (refreshToken, userInfoDoc) => {
+      try {
+        await updateDoc(userInfoDoc.ref, { refreshToken: refreshToken })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
     const unsubscribe = auth.onAuthStateChanged(async (userAuth) => {
       setLoading(false)
       if (userAuth) {
+        // userAuth contains the refresh token
         const userRef = await createUserProfileDocument(userAuth)
-        onSnapshot(userRef, (snapshot) => {
+        onSnapshot(userRef, async (snapshot) => {
           const snapshotData = snapshot.data()
-          const missingFieldValuePairs =
-            getMissingFieldValuePairsInUser(snapshotData)
-          if (Object.keys(missingFieldValuePairs).length > 0) {
-            updateDoc(userRef, missingFieldValuePairs)
+          const userInfo = await getUserInfo(snapshot.id)
+          if (userInfo.empty === true && userInfo.failed === false) {
+            await populateUserInfo(snapshot.id, userAuth.refreshToken)
+          } else if (userInfo.empty === false && userInfo.failed === false) {
+            if (
+              userInfo.userInfoDoc.data().refreshToken !== userAuth.refreshToken
+            ) {
+              console.log('refresh token changed')
+              await updateRefreshToken(
+                userAuth.refreshToken,
+                userInfo.userInfoDoc,
+              )
+            }
+          } else {
+            console.log('error getting user info')
+            alert('Please refresh the page')
           }
+
           const user = {
             displayName: snapshotData.displayName,
             email: snapshotData.email,
@@ -129,9 +193,6 @@ export const AuthProvider = ({ children }) => {
     return () => unsubscribe()
   }, [])
 
-  // useEffect(() => {
-  //   return !currentUser ? navigate("/") : null;
-  // }, [currentUser]);
   const authValue = {
     currentUser,
     signupWithEmail,
