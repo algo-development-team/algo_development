c903301ead4710488fb4d161b9f5aebb84661678
Hokyung (Andy) Lee <techandy42@gmail.com>
2022-11-13 07:57:11 -0500

    removed refresh token code

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -117,7 +117,6 @@ export const AuthProvider = ({ children }) => {
     const getDefaultUserInfo = () => {
       const defaultRankings = new Array(24).fill(50)
       const defaultUserInfo = {
-        refreshToken: null,
         workTimeRange: '9:00-17:00',
         sleepTimeRange: '23:00-07:00',
         workDays: [false, true, true, true, true, true, false],
