96835f1824c2ee09a3eb41b535b0a6ec037342ec
Hokyung (Andy) Lee <techandy42@gmail.com>
2022-11-17 14:30:59 -0500

    implemented the time range setting + validation checks

--- frontend/src/components/SettingEditor/index.jsx
+++ frontend/src/components/SettingEditor/index.jsx
@@ -18,8 +18,18 @@ import './styles/main.scss'
 import './styles/light.scss'
 import { getUserInfo } from '../../handleUserInfo'
 
-// note: build the logic first, then deal with error handling
-// note: implement a time handler function
+// note:
+// 1. logic and updating fields
+// 2. handling update to Firebase
+// 3. error handling
+// 4. styling
+
+const timeRangeType = Object.freeze({
+  sleepStart: 0,
+  sleepEnd: 1,
+  workStart: 2,
+  workEnd: 3,
+})
 
 export const SettingEditor = ({ closeOverlay }) => {
   const { currentUser } = useAuth()
@@ -56,9 +66,11 @@ export const SettingEditor = ({ closeOverlay }) => {
     const sleepTimesData = userInfo.sleepTimeRange
       .split('-')
       .map((time) => time.split(':'))
+      .map((time) => time.map((hourMin) => parseInt(hourMin)))
     const workTimesData = userInfo.workTimeRange
       .split('-')
       .map((time) => time.split(':'))
+      .map((time) => time.map((hourMin) => parseInt(hourMin)))
     setSleepStartTimeHour(sleepTimesData[0][0])
     setSleepStartTimeMin(sleepTimesData[0][1])
     setSleepEndTimeHour(sleepTimesData[1][0])
@@ -87,6 +99,187 @@ export const SettingEditor = ({ closeOverlay }) => {
     // update user info here
   }
 
+  const applyChangeTime = (hour, min, timeRangeTypeVal) => {
+    switch (timeRangeTypeVal) {
+      case timeRangeType.sleepStart:
+        setSleepStartTimeHour(hour)
+        setSleepStartTimeMin(min)
+        break
+      case timeRangeType.sleepEnd:
+        setSleepEndTimeHour(hour)
+        setSleepEndTimeMin(min)
+        break
+      case timeRangeType.workStart:
+        setWorkStartTimeHour(hour)
+        setWorkStartTimeMin(min)
+        break
+      case timeRangeType.workEnd:
+        setWorkEndTimeHour(hour)
+        setWorkEndTimeMin(min)
+        break
+      default:
+        break
+    }
+  }
+
+  const getTimeFromTimeRangeType = (timeRangeTypeVal) => {
+    let hour = 0
+    let min = 0
+    if (timeRangeTypeVal === timeRangeType.sleepStart) {
+      hour = sleepStartTimeHour
+      min = sleepStartTimeMin
+    } else if (timeRangeTypeVal === timeRangeType.sleepEnd) {
+      hour = sleepEndTimeHour
+      min = sleepEndTimeMin
+    } else if (timeRangeTypeVal === timeRangeType.workStart) {
+      hour = workStartTimeHour
+      min = workStartTimeMin
+    } else if (timeRangeTypeVal === timeRangeType.workEnd) {
+      hour = workEndTimeHour
+      min = workEndTimeMin
+    }
+    return { hour: hour, min: min }
+  }
+
+  const calcChangeTime = (isInc, isHour, timeRangeTypeVal) => {
+    let { hour, min } = getTimeFromTimeRangeType(timeRangeTypeVal)
+    if (isInc && isHour) {
+      hour += 1
+    } else if (!isInc && isHour) {
+      hour -= 1
+    } else if (isInc && !isHour) {
+      min += 15
+    } else if (!isInc && !isHour) {
+      min -= 15
+    }
+
+    if (min > 45) {
+      hour += 1
+      min = 0
+    } else if (min < 0) {
+      hour -= 1
+      min = 45
+    }
+    if (hour > 23) {
+      hour = 0
+    } else if (hour < 0) {
+      hour = 23
+    }
+    return { hour: hour, min: min }
+  }
+
+  const isBetween = (
+    hour,
+    min,
+    timeRangeTypeValFirst,
+    timeRangeTypeValSecond,
+    isInclusive,
+  ) => {
+    let totalMinTest = hour * 60 + min
+    const timeFirst = getTimeFromTimeRangeType(timeRangeTypeValFirst)
+    let totalMinFirst = timeFirst.hour * 60 + timeFirst.min
+    const timeSecond = getTimeFromTimeRangeType(timeRangeTypeValSecond)
+    const totalMinSecond = timeSecond.hour * 60 + timeSecond.min
+
+    if (totalMinFirst > totalMinSecond) {
+      totalMinFirst -= 1440
+    }
+    if (totalMinTest > totalMinSecond) {
+      totalMinTest -= 1440
+    }
+    if (isInclusive) {
+      return totalMinTest >= totalMinFirst && totalMinTest <= totalMinSecond
+    } else {
+      return totalMinTest > totalMinFirst && totalMinTest < totalMinSecond
+    }
+  }
+
+  const validateChangeTime = (hour, min, timeRangeTypeVal) => {
+    if (timeRangeTypeVal === timeRangeType.sleepStart) {
+      return (
+        !isBetween(
+          hour,
+          min,
+          timeRangeType.workStart,
+          timeRangeType.workEnd,
+          true,
+        ) &&
+        isBetween(
+          hour,
+          min,
+          timeRangeType.workEnd,
+          timeRangeType.sleepEnd,
+          false,
+        )
+      )
+    } else if (timeRangeTypeVal === timeRangeType.sleepEnd) {
+      return (
+        !isBetween(
+          hour,
+          min,
+          timeRangeType.workEnd,
+          timeRangeType.sleepStart,
+          true,
+        ) &&
+        isBetween(
+          hour,
+          min,
+          timeRangeType.sleepStart,
+          timeRangeType.workStart,
+          false,
+        )
+      )
+    } else if (timeRangeTypeVal === timeRangeType.workStart) {
+      return (
+        !isBetween(
+          hour,
+          min,
+          timeRangeType.sleepStart,
+          timeRangeType.sleepEnd,
+          true,
+        ) &&
+        isBetween(
+          hour,
+          min,
+          timeRangeType.sleepEnd,
+          timeRangeType.workEnd,
+          false,
+        )
+      )
+    } else if (timeRangeTypeVal === timeRangeType.workEnd) {
+      return (
+        !isBetween(
+          hour,
+          min,
+          timeRangeType.sleepEnd,
+          timeRangeType.workStart,
+          true,
+        ) &&
+        isBetween(
+          hour,
+          min,
+          timeRangeType.workStart,
+          timeRangeType.sleepStart,
+          false,
+        )
+      )
+    }
+  }
+
+  const changeTime = (isInc, isHour, timeRangeTypeVal) => {
+    const changeInTime = calcChangeTime(isInc, isHour, timeRangeTypeVal)
+    const validationResult = validateChangeTime(
+      changeInTime.hour,
+      changeInTime.min,
+      timeRangeTypeVal,
+    )
+    if (validationResult) {
+      applyChangeTime(changeInTime.hour, changeInTime.min, timeRangeTypeVal)
+    } else {
+      // error handling
+    }
+  }
+
   return (
     <div
       className={'add-task__wrapper quick-add__wrapper'}
@@ -101,45 +294,96 @@ export const SettingEditor = ({ closeOverlay }) => {
       >
         <div className={'add-task__actions quick-add__actions'}>
           <div>
-            <input
-              type='number'
-              value={sleepStartTimeHour}
-              onChange={(event) => {
-                handleChange(event)
-                setSleepStartTimeHour(event.target.value)
-              }}
-            />
-            <input
-              type='number'
-              step='15'
-              min='0'
-              max='60'
-              readonly
-              value={sleepStartTimeMin}
-              onChange={(event) => {
-                handleChange(event)
-                setSleepStartTimeMin(event.target.value)
-              }}
-            />
-            <input
-              type='number'
-              value={sleepEndTimeHour}
-              onChange={(event) => {
-                handleChange(event)
-                setSleepEndTimeHour(event.target.value)
-              }}
-            />
-            <input
-              type='number'
-              step='15'
-              min='0'
-              max='60'
-              value={sleepEndTimeMin}
-              onChange={(event) => {
-                handleChange(event)
-                setSleepEndTimeMin(event.target.value)
-              }}
-            />
+            <span>{sleepStartTimeHour}</span>
+            <button
+              onClick={() => changeTime(true, true, timeRangeType.sleepStart)}
+            >
+              up
+            </button>
+            <button
+              onClick={() => changeTime(false, true, timeRangeType.sleepStart)}
+            >
+              down
+            </button>
+            <span>{sleepStartTimeMin}</span>
+            <button
+              onClick={() => changeTime(true, false, timeRangeType.sleepStart)}
+            >
+              up
+            </button>
+            <button
+              onClick={() => changeTime(false, false, timeRangeType.sleepStart)}
+            >
+              down
+            </button>
+            <span>{sleepEndTimeHour}</span>
+            <button
+              onClick={() => changeTime(true, true, timeRangeType.sleepEnd)}
+            >
+              up
+            </button>
+            <button
+              onClick={() => changeTime(false, true, timeRangeType.sleepEnd)}
+            >
+              down
+            </button>
+            <span>{sleepEndTimeMin}</span>
+            <button
+              onClick={() => changeTime(true, false, timeRangeType.sleepEnd)}
+            >
+              up
+            </button>
+            <button
+              onClick={() => changeTime(false, false, timeRangeType.sleepEnd)}
+            >
+              down
+            </button>
+          </div>
+          <div>
+            <span>{workStartTimeHour}</span>
+            <button
+              onClick={() => changeTime(true, true, timeRangeType.workStart)}
+            >
+              up
+            </button>
+            <button
+              onClick={() => changeTime(false, true, timeRangeType.workStart)}
+            >
+              down
+            </button>
+            <span>{workStartTimeMin}</span>
+            <button
+              onClick={() => changeTime(true, false, timeRangeType.workStart)}
+            >
+              up
+            </button>
+            <button
+              onClick={() => changeTime(false, false, timeRangeType.workStart)}
+            >
+              down
+            </button>
+            <span>{workEndTimeHour}</span>
+            <button
+              onClick={() => changeTime(true, true, timeRangeType.workEnd)}
+            >
+              up
+            </button>
+            <button
+              onClick={() => changeTime(false, true, timeRangeType.workEnd)}
+            >
+              down
+            </button>
+            <span>{workEndTimeMin}</span>
+            <button
+              onClick={() => changeTime(true, false, timeRangeType.workEnd)}
+            >
+              up
+            </button>
+            <button
+              onClick={() => changeTime(false, false, timeRangeType.workEnd)}
+            >
+              down
+            </button>
           </div>
           <button
             className=' action add-task__actions--add-task'
